39462dedf71407fbd52f267382463c8c6164d16c
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-09-12 15:45:22 +0530

    refactor(IEmailService): remove sendEmailToConsultant method
    - Removed sendEmailToConsultant from the interface as it will be handled internally.

--- src/main/java/com/pmvyas/emailconsultation/service/IEmailService.java
+++ src/main/java/com/pmvyas/emailconsultation/service/IEmailService.java
@@ -2,5 +2,4 @@ package com.pmvyas.emailconsultation.service;
 
 public interface IEmailService {
     void sendEmailToClient(String clientName, String clientEmailId, String subject, String body);
-    void sendEmailToConsultant(String consultantName, String subject, String body, String clientEmailId);
 }

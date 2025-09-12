7d3ee8c734f8bc0fdb5a48f19c452146da1eab91
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-09-12 15:01:25 +0530

    refactor(IEmailService): update sendEmailToClient and add sendEmailToConsultant
    - Corrected parameter names in sendEmailToClient for better readability and maintainability
    - Added sendEmailToConsultant method to notify consultants about new consultation requests

--- src/main/java/com/pmvyas/emailconsultation/service/IEmailService.java
+++ src/main/java/com/pmvyas/emailconsultation/service/IEmailService.java
@@ -1,5 +1,6 @@
 package com.pmvyas.emailconsultation.service;
 
 public interface IEmailService {
-    void sendEmail(String name, String recipient, String subject, String body);
+    void sendEmailToClient(String clientName, String clientEmailId, String subject, String body);
+    void sendEmailToConsultant(String consultantName, String subject, String body, String clientEmailId);
 }

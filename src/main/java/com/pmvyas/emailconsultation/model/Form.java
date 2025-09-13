c80eaefcb5ba9950644e51740c928dded5a141b8
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-09-13 18:02:21 +0530

    feat(model): added validation annotations to fields of Form model class
    - Modified Form model class to include validation annotations.
    - Ensures input fields are validated (e.g., @NotBlank, @Email, @Size) before processing.
    - Improves data integrity and reduces invalid submissions.

--- src/main/java/com/pmvyas/emailconsultation/model/Form.java
+++ src/main/java/com/pmvyas/emailconsultation/model/Form.java
@@ -1,5 +1,8 @@
 package com.pmvyas.emailconsultation.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 /**
@@ -8,8 +11,20 @@ import lombok.Data;
  */
 @Data
 public class Form {
+
+    @NotBlank(message = "Full name is required.")
+    @Size(min = 10, max = 50, message = "Full name must be between 10 characters and 50 characters.")
     private String name;
+
+    @NotBlank(message = "Email is required.")
+    @Email(message = "Please provide a valid email address.")
     private String emailId;
+
+    @NotBlank(message = "Enter the subject of the request.")
+    @Size(max = 100, message = "Subject cannot exceed 100 characters.")
     private String subject;
+
+    @NotBlank(message = "Enter the details of your request.")
+    @Size(min = 100, max = 500, message = "Request details must be between 100 characters and 500 characters.")
     private String body;
 }

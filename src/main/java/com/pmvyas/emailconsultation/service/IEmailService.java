743ef2651e7be72002c735e439633baf538834ac
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-09-20 22:13:46 +0530

    refactor(IEmailService): change method return type from String to void
    - Updated method signature to return void instead of String

--- src/main/java/com/pmvyas/emailconsultation/service/IEmailService.java
+++ src/main/java/com/pmvyas/emailconsultation/service/IEmailService.java
@@ -1,5 +1,5 @@
 package com.pmvyas.emailconsultation.service;
 
 public interface IEmailService {
-    String bookConsultation(String clientName, String clientEmailId, String subject, String body);
+    void bookConsultation(String clientName, String clientEmailId, String subject, String body);
 }
